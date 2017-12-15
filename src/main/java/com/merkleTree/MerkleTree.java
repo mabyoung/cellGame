deb8f978e8e53a6e3fa5cbccd073cde0e2850ddd
rui <349763010@qq.com>
2017-12-15 21:24:13 +0800

    修改tree

--- src/main/java/com/merkleTree/MerkleTree.java
+++ src/main/java/com/merkleTree/MerkleTree.java
@@ -2,6 +2,7 @@ package com.merkleTree;
 
 import javax.sound.sampled.Line;
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.*;
 
 public class MerkleTree {
@@ -39,6 +40,35 @@ public class MerkleTree {
 
     }
 
+    public String md5sum(String data){
+        String fn = root+"\\"+data;
+        File f = new File(fn);
+        if (f.isFile()){
+            // 以字节流方法读取文件
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(f);
+                // 设置一个，每次 装载信息的容器
+                byte[] buf = new byte[1024];
+                // 定义一个StringBuffer用来存放字符串
+                StringBuffer sb = new StringBuffer();
+                // 开始读取数据
+                int len = 0;// 每次读取到的数据的长度
+                while ((len = fis.read(buf)) != -1) {// len值为-1时，表示没有数据了
+                    // append方法往sb对象里面添加数据
+                    sb.append(new String(buf, 0, len, "utf-8"));
+                }
+                // 输出字符串
+                System.out.println(sb.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while(true){
+                String d = f.read
+            }
+        }
+    }
+
     public List<String> getItems(String directory){
         List<String> value = new ArrayList<>();
         if (directory != this.root){
@@ -71,6 +101,7 @@ public class MerkleTree {
                 hashListChild(item);
             } else {
                 if (rootDir == this.root){
+                    hashList.put(item,md5sum(item));
                 }
             }
         }
